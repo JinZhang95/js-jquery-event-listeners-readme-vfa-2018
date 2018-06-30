570dfd11f3a56c7d34bec9abe50743b8db85c1bb
Jin Zhang <jnfzhang1995@gmail.com>
2018-06-30 00:44:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,13 @@ $(document).ready(function(){
 // call functions here
 
 });
+function getIt ()
+$('p').on("click",function(){"Hey!"});
+function frameIt ()
+$('.tasty').on('load', function(){ "tasty"});
+function pressIt()
+$(document).on('keydown',function(key) {if(key.which == 'G') {alert ("'s was pressed");
+}
+});
+function submitIt()
+$("form").on("submit", function() {"Your form is going to be submitted now."});
